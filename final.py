c3cdbc056f7fe7daf9a2c4359c0e7eaabba0e3a5
BufferFis <samaroct05@gmail.com>
2025-06-27 00:27:14 +0530

    eos things

--- final.py
+++ final.py
@@ -1003,10 +1003,10 @@ def main():
                     # ---------------- SplitFM beam-search settings -----------------
                     max_new_tokens        = 64,     # --eval_len
                     num_beams             = 10,     # --beam
-                    length_penalty        = 0.6,    # --length_penalty
+                    length_penalty        = 0.7,    # --length_penalty
                     early_stopping        = True,   # end once all beams hit <eos>
-                    no_repeat_ngram_size  = 3,      # --no_repeat_ngram_size
-                    repetition_penalty    = 1.4,    # --repetition_penalty (= neutral)
+                    no_repeat_ngram_size  = 4,      # --no_repeat_ngram_size
+                    repetition_penalty    = 1.3,    # --repetition_penalty (= neutral)
                     diversity_penalty   = 0.3,
                     num_beam_groups     = 5,   # must divide num_beams (10)
 
@@ -1018,7 +1018,8 @@ def main():
 
                     # housekeeping
                     remove_invalid_values = True,   # robust against any NaNs
-                    eos_token_id          = trainer.tokenizer.eos_token_id,  # SplitFM passes 628
+                    #eos_token_id          = trainer.tokenizer.eos_token_id,  # SplitFM passes 628
+                    eos_token_id = 628,
                     pad_token_id          = trainer.tokenizer.pad_token_id,
                     return_dict_in_generate = False # SplitFM just wants the ids
             )
