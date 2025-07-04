46834f4e7449a3289eab0365d949b8c51df1a9c8
BufferFis <samaroct05@gmail.com>
2025-07-04 17:05:17 +0530

    beam fix

--- final.py
+++ final.py
@@ -875,7 +875,7 @@ def generate_with_sampling(trainer, wrapper, mr_text, ref_text, max_new_tokens=4
         output = wrapper.generate(
             ids,
             attention_mask=m,
-            num_beams = 1,  # Use single beam for sampling
+            num_beams = 5,  # Use single beam for sampling
             # SAMPLING APPROACH (avoids beam search repetition)
             max_new_tokens=30,
             do_sample=True,
