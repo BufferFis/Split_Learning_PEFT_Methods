b53dd3e2b5c5489a59abc1dea2a1c6dc4359ee89
BufferFis <samaroct05@gmail.com>
2025-08-10 17:22:04 +0530

    BLEU 63 LETS GOO

--- nosplitgpt2.py
+++ nosplitgpt2.py
@@ -758,13 +758,13 @@ def evaluate_model(args, model, tokenizer, eval_dataloader, eval_dataset):
                     outputs = model.generate(
                         input_ids=input_ids,
                         attention_mask=attention_mask,
-                        max_length=input_ids.shape[1] + 25,
+                        max_length=input_ids.shape[1] + 15,
                         num_beams=8,
                         num_return_sequences=5,  # Generate 5 candidates
                         early_stopping=True,
                         no_repeat_ngram_size=4,
-                        repetition_penalty=1.25,
-                        length_penalty=0.8,
+                        repetition_penalty=1.3,
+                        length_penalty=0.6,
                         pad_token_id=tokenizer.eos_token_id,
                         eos_token_id=tokenizer.eos_token_id,
                     )
