03d9f4849043880dbf963d7155601ef6a27c7cce
BufferFis <samaroct05@gmail.com>
2025-08-11 22:18:49 +0530

    just one more token

--- nosplitgpt2.py
+++ nosplitgpt2.py
@@ -759,7 +759,7 @@ def evaluate_model(args, model, tokenizer, eval_dataloader, eval_dataset):
             gen_kwargs = {
                     "input_ids": input_ids,
                     "attention_mask": attention_mask,
-                    "max_length": input_ids.shape[1] + 15,      # Reduced from 25
+                    "max_length": input_ids.shape[1] + 17,      # Reduced from 25
                     "num_beams": 8,
                     "num_return_sequences": 5,
                     "early_stopping": True,
