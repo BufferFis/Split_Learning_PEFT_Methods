5c4e218b34117923921c5973a28dc50b252dc507
BufferFis <samaroct05@gmail.com>
2025-08-13 10:05:07 +0530

    New gen params added

--- nosplitgpt2.py
+++ nosplitgpt2.py
@@ -921,13 +921,13 @@ def evaluate_model(args, model, tokenizer, eval_dataloader, eval_dataset):
             gen_kwargs = {
                     "input_ids": input_ids,
                     "attention_mask": attention_mask,
-                    "max_length": input_ids.shape[1] + 23,      # Reduced from 25
+                    "max_length": input_ids.shape[1] + 25,      # Reduced from 25
                     "num_beams": 8,
                     "num_return_sequences": 5,
                     "early_stopping": True,
                     "no_repeat_ngram_size": 4,
-                    "repetition_penalty": 1.2,                  # Reduced from 1.25
-                    "length_penalty": 1,                      
+                    "repetition_penalty": 1.25,                  # Reduced from 1.25
+                    "length_penalty": 0.8,                      
                     "pad_token_id": tokenizer.eos_token_id,
                     "eos_token_id": tokenizer.eos_token_id,
                 }
